25ec6833ca6b91a8f2d03a3d39d886b7392874a2
Sithranjan Suresh <sithranjan.suresh@gmail.com>
2026-07-17 22:05:33 -0500

    Replace generic panda emoji with custom Higgsfield-illustrated avatar art

--- src/components/PandaAvatar.jsx
+++ src/components/PandaAvatar.jsx
@@ -1,13 +1,13 @@
-const STATE_EMOJI = {
-  thinking: '🐼🎋',
-  delivering: '🐼',
-  refusing: '🐼💤',
+const STATE_BADGE = {
+  thinking: '🎋',
+  refusing: '💤',
 };
 
 export default function PandaAvatar({ state = 'delivering', className = '' }) {
   return (
     <div className={`panda-avatar panda-avatar--${state} ${className}`} aria-hidden="true">
-      {STATE_EMOJI[state] ?? STATE_EMOJI.delivering}
+      <img className="panda-avatar__img" src="/film/panda-icon.webp" alt="" />
+      {STATE_BADGE[state] && <span className="panda-avatar__badge">{STATE_BADGE[state]}</span>}
     </div>
   );
 }
